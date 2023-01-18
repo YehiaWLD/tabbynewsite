bd71899aa9a7337026c50fb6d953611b37bd7246
yeehaia <66638625+YehiaWLD@users.noreply.github.com>
2023-01-18 11:27:41 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ navigator.geolocation.getCurrentPosition(position => {
             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
             document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
-                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-temp">${Math.round(data.main.temp)}ºf</p>
                 <p class="weather-city">${data.name}</p>
             `
         })
